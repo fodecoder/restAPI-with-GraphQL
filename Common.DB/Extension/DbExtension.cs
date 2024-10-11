0d9d75bca34fe6e7ab86b9df00d858413657b281
Andrea Foderaro <fodecoder@gmail.com>
2024-10-11 13:52:24 +0200

    Add Common DB Library

---
+++ Common.DB/Extension/DbExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.DB.Extension
+{
+    public static class DbExtension
+    {
+        public static IServiceCollection AddDatabase<T>( this IServiceCollection services ) where T : DbContext
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath ( folder );
+            var DbPath = Path.Join ( path , "inventory.db" );
+
+            // The following configures EF to create a Sqlite database file in the
+            // special "local" folder for your platform.
+            return services.AddDbContext<T> (
+                options => options.UseSqlite ( $"Data Source={DbPath}" ) );
+        }
+    }
+}
---
+++ Common.DB/Model/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.DB.Model
+{
+    public class BaseEntity
+    {
+        // add basic info to all the database entities
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+        public BaseEntity()
+        {
+            Created = DateTime.UtcNow;
+            Modified = DateTime.UtcNow;
+        }
+    }
+}
