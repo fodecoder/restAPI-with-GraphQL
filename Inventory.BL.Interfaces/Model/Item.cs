b69964fe572ffdfc1f2d6713832f5ff51ee1f256
Andrea Foderaro <fodecoder@gmail.com>
2024-10-14 17:14:33 +0200

    Add classes for GraphQL

--- Inventory.BL.Interfaces/Model/Item.cs
+++ Inventory.BL.Interfaces/Model/Item.cs
@@ -3,8 +3,8 @@
     public class Item
     {
         public Guid Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
     }
 }
---
+++ Inventory.BL.Interfaces/Model/ItemData.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace Inventory.BL.Interfaces.Model
+{
+    public class ItemData : ObjectGraphType<Item>
+    {
+        public ItemData()
+        {
+            Field(x => x.Id).Description("The Id of the Item.");
+            Field(x => x.Name).Description("The name of the Item.");
+            Field(x => x.Description).Description("The description of the Item.");
+            Field(x => x.Type).Description("The type of the Item.");
+        }
+    }
+}
