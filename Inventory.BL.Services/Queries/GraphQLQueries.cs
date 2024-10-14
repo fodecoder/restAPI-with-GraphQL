0afc270a45fdbd4d83c9552931be8681f15e48b1
Andrea Foderaro <fodecoder@gmail.com>
2024-10-14 17:14:57 +0200

    Add services for graphQL

---
+++ Inventory.BL.Services/Queries/GraphQLQueries.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using Inventory.BL.Interfaces.Model;
+
+namespace Inventory.BL.Services.Queries
+{
+    public class InventoryQuery : ObjectGraphType
+    {
+        private List<ItemData> items = new List<ItemData> ();
+
+        public InventoryQuery()
+        {
+            Field<ListGraphType<ItemData>> ( "items" )
+                .Description ( "Returns a collection of items." )
+                .Resolve ( context => items );
+        }
+    }
+}
