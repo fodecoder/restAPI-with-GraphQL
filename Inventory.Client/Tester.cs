3e29694193742a6c537ecd6963fba35eb51fa4b1
Andrea Foderaro <fodecoder@gmail.com>
2024-10-18 16:39:55 +0200

    Add other tests

--- Inventory.Client/Tester.cs
+++ Inventory.Client/Tester.cs
@@ -23,6 +23,19 @@ namespace Inventory.Client
                     Description = "Description - " + new Random ().Next ( 0 , 10000 ) ,
                     Type = "Type - " + new Random ().Next ( 0 , 10 )
                 } );
+
+            Console.WriteLine ( "Test Read..." );
+            await _itemService.ReadItemAsync ( Guid.Parse ( "D71E936A-AABB-4A9D-B26D-0474802E8406" ) );
+
+            Console.WriteLine ( "Test Update..." );
+            await _itemService.UpdateItemAsync (
+                new BL.Interfaces.Model.Item ()
+                {
+                    Id = Guid.Parse ( "D71E936A-AABB-4A9D-B26D-0474802E8406" ) ,
+                    Name = "Test - " + new Random ().Next ( 0 , 10000 ) ,
+                    Description = "Description - " + new Random ().Next ( 0 , 10000 ) ,
+                    Type = "Type - " + new Random ().Next ( 0 , 10 )
+                } );
         }
     }
 }
